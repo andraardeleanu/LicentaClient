d786f2a842fbe58310084dd04bfc847a9fd0ca49
avasiloaiecatalin <48545737+avasiloaiecatalin@users.noreply.github.com>
2024-04-02 17:49:09 +0300

    Fix + improvement

--- client/src/fragments/OrderFragments/CreateOrderFragment.jsx
+++ client/src/fragments/OrderFragments/CreateOrderFragment.jsx
@@ -51,14 +51,21 @@ export const CreateOrderFragment = ({ onClose, companyId }) => {
   const endOffset = itemOffset + 10;
   const currentItems = products.slice(itemOffset, endOffset);
   const pageCount = Math.ceil(products.length / 10);
-
+  const [totalPrice, setTotalPrice] = useState(0);
   const handlePageClick = (event) => {
     const newOffset = (event.selected * 10) % products.length;
     setItemOffset(newOffset);
   };
 
+  const recalculateTotal = (list) => {
+    let total = 0;
+    list.forEach((item) => {
+      total += item.price * item.quantity;
+    });
+    setTotalPrice(total);
+  };
 
-  const updateSelectedProducts = (id, qty) => {
+  const updateSelectedProducts = (id, qty, price) => {
     let bufferList = selectedProducts;
     const index = findProductIndexById(bufferList, id);
     if (index !== -1) {
@@ -69,9 +76,11 @@ export const CreateOrderFragment = ({ onClose, companyId }) => {
     if (qty && qty !== 0) {
       bufferList.push({
         productId: id,
-        quantity: qty
+        quantity: qty,
+        price: price
       });
     }
+    recalculateTotal(bufferList);
     setSelectedProducts(bufferList);
   };
 
@@ -120,137 +129,166 @@ export const CreateOrderFragment = ({ onClose, companyId }) => {
   });
 
   return (
-    <Formik
-      initialValues={{
-        workpointId: 0
-      }}
-      onSubmit={async (values) => {
-        setLoading(true);
-        const response = await createOrder(
-          {
-            author: data?.username,
-            createdBy: data?.id,
-            workpointId: values.workpointId,
-            fileType: 1,
-            products: selectedProducts
-          },
-          cookies.userToken
-        );
-        setLoading(false);
-        if (response.errorMessage) {
-          setUserError(response.errorMessage);
-        } else {
-          toast({
-            title: 'Comanda a fost creata cu succes!',
-            status: 'success',
-            duration: 5000,
-            isClosable: true,
-            position: 'top'
-          });
-          dispatch(setNeedOrdersCall(true));
-          navigate('/');
-          onClose()
-        }
-      }}
-    >
-      {({ handleSubmit, handleChange, values }) => (
-        <Form
-          onSubmit={handleSubmit}
-          onChange={() => {
-            setUserError('');
-          }}
-        >
-          <Stack
-            spacing={4}
-            className='mt-6'
+    <>
+      <Formik
+        initialValues={{
+          workpointId: 0
+        }}
+        onSubmit={async (values) => {
+          setLoading(true);
+          const response = await createOrder(
+            {
+              author: data?.username,
+              createdBy: data?.id,
+              workpointId: values.workpointId,
+              fileType: 1,
+              products: selectedProducts
+            },
+            cookies.userToken
+          );
+          setLoading(false);
+          if (response.errorMessage) {
+            setUserError(response.errorMessage);
+          } else {
+            toast({
+              title: 'Comanda a fost creata cu succes!',
+              status: 'success',
+              duration: 5000,
+              isClosable: true,
+              position: 'top'
+            });
+            dispatch(setNeedOrdersCall(true));
+            navigate('/');
+            onClose();
+          }
+        }}
+      >
+        {({ handleSubmit, handleChange, values }) => (
+          <Form
+            onSubmit={handleSubmit}
+            onChange={() => {
+              setUserError('');
+            }}
           >
-            <Select
-              id='workpointId'
-              name='workpointId'
-              placeholder='Selecteaza punct de lucru...'
-              onChange={handleChange}
-              isDisabled={workpointsLoading}
+            <Stack
+              spacing={4}
+              className='mt-6'
             >
-              {workpoints.map((wp) => (
-                <option value={wp?.id}>{wp?.name}</option>
-              ))}
-            </Select>
-            {!productsLoading && (
-              <TableContainer>
-                <Heading
-                  size={'sm'}
-                  mb={2}
-                >
-                  Produse
-                </Heading>
-                <Table size='sm'>
-                  <Thead>
-                    <Tr>
-                      <Th>Nume</Th>
-                      <Th>Pret</Th>
-                      <Th>Cantitate</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {currentItems.map((prod) => (
-                      <Tr key={prod?.id}>                        
-                        <Td>{prod?.name}</Td>
-                        <Td>{prod?.price}</Td>
-                        <Td>
-                          <Input
-                            className='max-w-[100px]'
-                            id='quantity'
-                            name='quantity'
-                            placeholder='0'
-                            type='number'
-                            defaultValue={0}
-                            onChange={(e) => {
-                              updateSelectedProducts(prod?.id, e.target.value);
-                            }}
-                          />
-                        </Td>
+              <Select
+                id='workpointId'
+                name='workpointId'
+                placeholder='Selecteaza punct de lucru...'
+                onChange={handleChange}
+                isDisabled={workpointsLoading}
+              >
+                {workpoints.map((wp) => (
+                  <option
+                    key={wp?.id}
+                    value={wp?.id}
+                  >
+                    {wp?.name}
+                  </option>
+                ))}
+              </Select>
+              {!productsLoading && (
+                <TableContainer>
+                  <Heading
+                    size={'sm'}
+                    mb={2}
+                  >
+                    Produse
+                  </Heading>
+                  <Table size='sm'>
+                    <Thead>
+                      <Tr>
+                        <Th>Nume</Th>
+                        <Th>Pret</Th>
+                        <Th>Cantitate</Th>
                       </Tr>
-                    ))}
-                  </Tbody>
-                </Table>
-                <Divider my={6} />
-                <ReactPaginate
-                  breakLabel='...'
-                  nextLabel='>'
-                  onPageChange={handlePageClick}
-                  pageRangeDisplayed={5}
-                  pageCount={pageCount}
-                  previousLabel='<'
-                  renderOnZeroPageCount={null}
-                  className='flex items-center gap-4 justify-center'
-                />
-              </TableContainer>
-            )}
-            {userError && (
-              <Alert status='error'>
-                <AlertIcon />
-                <AlertTitle>{userError}</AlertTitle>
-              </Alert>
-            )}
-            <Button
-              colorScheme='blue'
-              onClick={() => {
-                handleSubmit();
-                onClose();
-              }}
-              isLoading={loading}
-            >
-              Creeaza
-            </Button>
-            <Button
-              onClick={onClose}
-              disabled={loading}
-            >
-              Renunta
-            </Button>
-          </Stack>
-        </Form>
-      )}
-    </Formik>
+                    </Thead>
+                    <Tbody>
+                      {currentItems.map((prod) => (
+                        <Tr key={prod?.id}>
+                          <Td>{prod?.name}</Td>
+                          <Td>{prod?.price}</Td>
+                          <Td>
+                            <Input
+                              className='max-w-[100px]'
+                              id='quantity'
+                              name='quantity'
+                              placeholder='0'
+                              type='number'
+                              defaultValue={
+                                selectedProducts?.find(
+                                  (p) => p.productId === prod?.id
+                                ) !== -1
+                                  ? selectedProducts?.find(
+                                      (p) => p.productId === prod?.id
+                                    )?.quantity
+                                  : 0
+                              }
+                              onChange={(e) => {
+                                updateSelectedProducts(
+                                  prod?.id,
+                                  e.target.value,
+                                  prod?.price
+                                );
+                              }}
+                            />
+                          </Td>
+                        </Tr>
+                      ))}
+                    </Tbody>
+                  </Table>
+                  <Divider my={6} />
+                  <ReactPaginate
+                    breakLabel='...'
+                    nextLabel='>'
+                    onPageChange={handlePageClick}
+                    pageRangeDisplayed={5}
+                    pageCount={pageCount}
+                    previousLabel='<'
+                    renderOnZeroPageCount={null}
+                    className='flex items-center gap-4 justify-center'
+                  />
+                </TableContainer>
+              )}
+              {userError && (
+                <Alert status='error'>
+                  <AlertIcon />
+                  <AlertTitle>{userError}</AlertTitle>
+                </Alert>
+              )}
+              <div className='w-full'>
+                <Heading
+                  size={'md'}
+                  mb={2}
+                  className='flex items-center justify-between'
+                >
+                  <span>Pret total:</span>
+                  <span className='text-4xl'>{totalPrice}</span>
+                </Heading>
+              </div>
+              <Button
+                colorScheme='blue'
+                onClick={() => {
+                  handleSubmit();
+                  onClose();
+                }}
+                isLoading={loading}
+              >
+                Creeaza
+              </Button>
+              <Button
+                onClick={onClose}
+                disabled={loading}
+              >
+                Renunta
+              </Button>
+            </Stack>
+          </Form>
+        )}
+      </Formik>
+    </>
   );
 };
