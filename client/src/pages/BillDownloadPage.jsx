11efcf06b1d6aa6ab56b5f7b5edbd79cae85c538
andraardeleanu <and.andraa16@gmail.com>
2024-04-26 22:02:18 +0300

    bills

--- client/src/pages/BillDownloadPage.jsx
+++ client/src/pages/BillDownloadPage.jsx
@@ -1,11 +1,16 @@
 import {
-  Heading
+  Heading,
+  Text,
+  Divider,
+  GridItem,
+  Grid,
+  Kbd
 } from '@chakra-ui/react';
 import { useParams } from 'react-router-dom';
 import { AppContainer } from '../components/AppContainer';
 import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { getOrderDetailsForBill } from '../utils/apiCalls';
+import { getBillDetails } from '../utils/apiCalls';
 import { useCookies } from 'react-cookie';
 import { usePDF } from 'react-to-pdf';
 
@@ -31,7 +36,7 @@ export const BillDownloadPage = () => {
       try {
         if (needCall) {
           setLoading(true);
-          await getOrderDetailsForBill(cookies.userToken, orderId).then(
+          await getBillDetails(orderId, cookies.userToken).then(
             (res) => {
               setLoading(false);
               setOrderDetails(res);
@@ -68,12 +73,47 @@ export const BillDownloadPage = () => {
             {orderDetails?.orderNo}
           </Heading>
           <Heading
+            lineHeight='tall'
             size={'md'}
             mt={4}
             className='text-center'
           >
             Factura comanda {orderDetails?.orderNo}
           </Heading>
+          <Divider my={15} />
+          <Grid
+            templateAreas={`"header header"
+                  "nav main"
+                  "nav footer"`}
+            gridTemplateRows={'50px 1fr 30px'}
+            gridTemplateColumns={'150px 1fr'}
+            h='200px'
+            gap='1'
+            color='blackAlpha.700'
+            fontWeight='bold'
+          >
+            <GridItem pl='2' area={'header'}>
+              Detalii factura
+              <Text mt='6' fontWeight='bold'>
+                Total price - {orderDetails?.totalPrice}
+              </Text>
+              <Text mt='6' fontWeight='bold'>
+                Numar comanda - {orderDetails?.orderNo}
+              </Text>
+            </GridItem>
+            <GridItem pl='2' area={'footer'}>
+              Detalii client
+              <Text mt='6' fontWeight='bold'>
+                Client - {orderDetails?.author}
+              </Text>
+              <Text mt='6' fontWeight='bold'>
+                Companie - {orderDetails?.companyName}
+              </Text>
+              <Text mt='6' fontWeight='bold'>
+                Punct de lucru - {orderDetails?.workpointName}
+              </Text>
+            </GridItem>
+          </Grid>
         </div>
       )}
     </AppContainer>
